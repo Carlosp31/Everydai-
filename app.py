3f2bc7f5aceba63529c0fc55b8c386d27a4d3f32
unknown <cpardobar@gmail.com>
2025-01-16 19:29:03 -0500

    Pose animation added to gym avatar

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ from google_auth_oauthlib.flow import Flow
 from typing_extensions import override
 from openai import AssistantEventHandler
 # Cargar las variables de entorno del archivo .env
+
+
+
+
 load_dotenv()
 cert_file = '/etc/letsencrypt/live/everydai.ddns.net/fullchain.pem'
 key_file = '/etc/letsencrypt/live/everydai.ddns.net/privkey.pem'
