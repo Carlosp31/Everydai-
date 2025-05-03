bc9c0d36caf93d736948647b97a4ef93860f1277
unknown <cpardobar@gmail.com>
2025-05-02 19:37:10 -0500

    frontend changes

--- static/java/chat.js
+++ static/java/chat.js
@@ -446,7 +446,7 @@ recommendationsList.innerHTML = '';  // Limpiar la lista existente
             window.startTimer = function(type) {
                 return { type: type, startTime: Date.now() };
             }
-            //window.sendMessage("Hola")
+            window.sendMessage("Hola")
             // Función para registrar el tiempo de respuesta
             window.logResponse = function(interaction, responseType) {
                 interaction.endTime = Date.now();
